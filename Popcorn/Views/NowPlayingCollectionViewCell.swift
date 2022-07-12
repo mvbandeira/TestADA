fa2d53de05689a64bc58ca4fb397b4d984d6f341
mvbsa <mvbsa@academy.cin.ufpe.br>
2022-07-12 18:04:06 -0300

    aula 12 de julho

--- Popcorn/Views/NowPlayingCollectionViewCell.swift
+++ Popcorn/Views/NowPlayingCollectionViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class NowPlayingCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet var image: UIImageView!
+    @IBOutlet var imageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     
